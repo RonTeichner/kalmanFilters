bc3461100d4a38fb617cee249c8c099302686c1b
Ron Teichner <ron.teichner@campus.technion.ac.il>
2019-09-07 00:09:57 +0300

    measNoise and processNoise analysis

--- filterpy/kalman/tests/filtering_vs_smoothing.py
+++ filterpy/kalman/tests/filtering_vs_smoothing.py
@@ -2,11 +2,14 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import numpy as np
 from numpy import random
-import matplotlib.pyplot as plt
+import pylab as plt
+#import matplotlib.pyplot as plt
 from filterpy.kalman import KalmanFilter, update, predict, batch_filter
 import scipy.io as sio
 from tempfile import TemporaryFile
 from filtering_vs_smoothing_Func import *
+
+True_testMeasNoiseChange_False_testProcessNoiseChange = False
 
 mat_contents = sio.loadmat('../../../../traffic/mcRun01/sDataset.mat')
 sDatasetRnn = mat_contents['sDatasetRnn']
@@ -28,30 +31,66 @@ F[-12,-1] = 1
 nMonteCarlo = 100
 
 enablePlot = True
-
+measNoiseTrueOrProcessNoiseFalse = False
 #desiredInputMeasNoise_dbm = 100
 a = -10
-#desiredOutputMeasNoiseVec_dbm = np.arange(a, a+1)#np.arange(-35, 5)
-desiredOutputMeasNoiseVec_dbm = np.arange(-35, 15, 1)
-nItrs = desiredOutputMeasNoiseVec_dbm.size
+if True_testMeasNoiseChange_False_testProcessNoiseChange:
+    #a= -10
+    #desiredOutputMeasNoiseVec_dbm = np.arange(a, a+1)#np.arange(-35, 5)
+    desiredOutputMeasNoiseVec_dbm = np.arange(-35, 15, 1)
+    desiredProcessNoise_dbm = watt2dbm(Q[1,1])
+    nItrs = desiredOutputMeasNoiseVec_dbm.size
+    feature = -10
+else:
+    F = np.array([[1, 0], [1,0]], dtype=float)
+    H = np.array([[0, 1]], dtype=float)
+    Q = np.array(np.diag([0,0]), dtype=float)
+    desiredOutputMeasNoiseVec_dbm = -0
+    a = -60
+    desiredProcessNoise_dbm = np.arange(a , a + 30, 2)
+    nItrs = desiredProcessNoise_dbm.size
+    feature = 0
+
+
 
 meanErrF2ErrS_db = np.zeros(nItrs)
 meanErrS2ErrF_db = np.zeros(nItrs)
 outputMeasSnrPower_db = np.zeros(nItrs)
+processMeanSnr_db = np.zeros(nItrs)
 for itr in range(nItrs):
     print('starting general itr %d out of %d' %(itr, nItrs))
-    desiredInputMeasNoise_dbm = desiredOutputMeasNoiseVec_dbm[itr] + 6
-    meanErrF2ErrS_linear, meanErrS2ErrF_linear, outputMeasSnrPower_Linear = simFiltering2SmoothingErrorPower(desiredInputMeasNoise_dbm, desiredOutputMeasNoiseVec_dbm[itr], F, Q, H, nMonteCarlo, enablePlot)
+    if True_testMeasNoiseChange_False_testProcessNoiseChange:
+        desiredInputMeasNoise_dbm = desiredOutputMeasNoiseVec_dbm[itr] + 6
+        meanErrF2ErrS_linear, meanErrS2ErrF_linear, outputMeasSnrPower_Linear, processMeanSnr_Linear = simFiltering2SmoothingErrorPower(True, feature, desiredInputMeasNoise_dbm, desiredOutputMeasNoiseVec_dbm[itr], F, Q, H, nMonteCarlo, enablePlot)
+    else:
+        desiredInputMeasNoise_dbm = 100#desiredOutputMeasNoiseVec_dbm + 6
+        Q[0, 0] = dbm2var(desiredProcessNoise_dbm[itr])
+        meanErrF2ErrS_linear, meanErrS2ErrF_linear, outputMeasSnrPower_Linear, processMeanSnr_Linear = simFiltering2SmoothingErrorPower(False, feature, desiredInputMeasNoise_dbm, desiredOutputMeasNoiseVec_dbm, F, Q, H, nMonteCarlo, enablePlot)
+
     meanErrF2ErrS_db[itr] = 10*np.log10(np.mean(meanErrF2ErrS_linear))
     meanErrS2ErrF_db[itr] = 10*np.log10(np.mean(meanErrS2ErrF_linear))
     outputMeasSnrPower_db[itr] = 10*np.log10(np.mean(outputMeasSnrPower_Linear))
+    processMeanSnr_db[itr] = 10*np.log10(np.mean(processMeanSnr_Linear))
     #print('meanErrF2ErrS_db:', np.array_str(10*np.log10(meanErrF2ErrS_linear), precision=0, suppress_small=True))
     #print('outputMeasSnr_db:', np.array_str(10*np.log10(outputMeasSnrPower_Linear), precision=0, suppress_small=True))
+
+
+plt.plot(desiredProcessNoise_dbm, processMeanSnr_db)
+plt.xlabel('process noise [dbm]')
+plt.ylabel('[db]')
+plt.title('filtering2smoothing error (power ratio)')
+plt.show()
 
 sortedIdx = np.argsort(outputMeasSnrPower_db)
 outputMeasSnrPower_db = outputMeasSnrPower_db[sortedIdx]
 meanErrF2ErrS_db = meanErrF2ErrS_db[sortedIdx]
 meanErrS2ErrF_db = meanErrS2ErrF_db[sortedIdx]
+processMeanSnr_db = processMeanSnr_db[sortedIdx]
+desiredProcessNoise_dbm = desiredProcessNoise_dbm[sortedIdx]
+
+print(meanErrF2ErrS_linear)
+
+
 
 plt.subplot(2,1,1)
 plt.plot(outputMeasSnrPower_db, meanErrF2ErrS_db)
@@ -60,11 +99,19 @@ plt.ylabel('[db]')
 plt.title('filtering2smoothing error (power ratio)')
 
 plt.subplot(2,1,2)
-plt.plot(outputMeasSnrPower_db, meanErrS2ErrF_db)
-plt.xlabel('meas snr [db]')
+
+sortedIdx = np.argsort(processMeanSnr_db)
+outputMeasSnrPower_db = outputMeasSnrPower_db[sortedIdx]
+meanErrF2ErrS_db = meanErrF2ErrS_db[sortedIdx]
+meanErrS2ErrF_db = meanErrS2ErrF_db[sortedIdx]
+processMeanSnr_db = processMeanSnr_db[sortedIdx]
+
+plt.plot(processMeanSnr_db, meanErrF2ErrS_db)
+plt.xlabel('process snr [db]')
 plt.ylabel('[db]')
-plt.title('smoothing2filtering error (power ratio)')
+plt.title('filtering2smoothing error (power ratio)')
 plt.show()
 
 np.save('./meanErrF2ErrS_db_file.npy', meanErrF2ErrS_db)
 np.save('./outputMeasSnrPower_db_file.npy', outputMeasSnrPower_db)
+np.save('./processMeanSnr_db_file.npy', processMeanSnr_db)
--- filterpy/kalman/tests/filtering_vs_smoothing_Func.py
+++ filterpy/kalman/tests/filtering_vs_smoothing_Func.py
@@ -21,21 +21,31 @@ def watt2dbm(x_volt):
 def watt2db(x_volt):
     return 10*np.log10(x_volt)
 
-def simFiltering2SmoothingErrorPower(desiredInputMeasNoise_dbm, desiredOutputMeasNoise_dbm, F, Q, H, nBatch, enablePlot):
+def simFiltering2SmoothingErrorPower(case, feature, desiredInputMeasNoise_dbm, desiredOutputMeasNoise_dbm, F, Q, H, nBatch, enablePlot):
+    if not(case):
+        R = np.diag([dbm2var(desiredOutputMeasNoise_dbm)])
+    else:
+        R = np.diag([dbm2var(desiredInputMeasNoise_dbm), dbm2var(desiredOutputMeasNoise_dbm)])
+    inputNoiseVar = Q[0,0]
+    lowNoiseVar = Q[1,1]
 
-    R = np.diag([dbm2var(desiredInputMeasNoise_dbm), dbm2var(desiredOutputMeasNoise_dbm)])
 
     xDim = F.shape[0]
     measDim = H.shape[0]
 
+    Q = np.diag(np.concatenate((inputNoiseVar * np.ones(1), lowNoiseVar*np.ones(xDim-1))))
+
     Q_dbm = 10*np.log10(np.diag(Q)) + 30
     R_dbm = 10*np.log10(np.diag(R)) + 30
-
-    nSamples = 10000
+    if case:
+        nSamples = 10000
+    else:
+        nSamples = 10000
 
     meanErrF2ErrSPower_Linear = np.zeros(nBatch)
     meanErrS2ErrFPower_Linear = np.zeros(nBatch)
     outputMeasSnr_Linear = np.zeros(nBatch)
+    processMeanSnr_Linear = np.zeros(nBatch)
     for batchIdx in range(nBatch):
         if batchIdx % 10 == 0:
             print('starting iter %d' % batchIdx)
@@ -60,19 +70,30 @@ def simFiltering2SmoothingErrorPower(desiredInputMeasNoise_dbm, desiredOutputMea
         xGroundTruth_dbm = 10 * np.log10(np.power(xGroundTruth, 2)) + 30
         yGroundTruth_dbm = 10 * np.log10(np.power(yGroundTruth, 2)) + 30
         y_dbm = 10 * np.log10(np.power(y, 2)) + 30
+        if case:
+            inputNoisePower_dbm = 10*np.log10(np.diag(R)[0]) + 30
+            outputNoisePower_dbm = 10*np.log10(np.diag(R)[1]) + 30
+        else:
+            inputNoisePower_dbm = 10 * np.log10(np.diag(R)[0]) + 300
+            outputNoisePower_dbm = 10 * np.log10(np.diag(R)[0]) + 30
 
-        inputNoisePower_dbm = 10*np.log10(np.diag(R)[0]) + 30
-        outputNoisePower_dbm = 10*np.log10(np.diag(R)[1]) + 30
-        inputMeanSigPower_dbm = 10*np.log10(np.mean(np.power(yGroundTruth[:,0],2))) + 30
-        outputMeanSigPower_dbm = 10*np.log10(np.mean(np.power(yGroundTruth[:,1],2))) + 30
+        if case:
+            inputMeanSigPower_dbm = 10*np.log10(np.mean(np.power(yGroundTruth[:,0],2))) + 30
+            outputMeanSigPower_dbm = 10*np.log10(np.mean(np.power(yGroundTruth[:,1],2))) + 30
+        else:
+            inputMeanSigPower_dbm = 10 * np.log10(np.mean(np.power(yGroundTruth[:, 0], 2))) + 300
+            outputMeanSigPower_dbm = 10 * np.log10(np.mean(np.power(yGroundTruth[:, 0], 2))) + 30
         inputMeasSnr_db = inputMeanSigPower_dbm - inputNoisePower_dbm
         outputMeanSnr_db = outputMeanSigPower_dbm - outputNoisePower_dbm
-
-        outputMeasSnr_Linear[batchIdx] = np.mean(np.divide(np.power(yGroundTruth[:, 1], 2), np.diag(R)[1]))
+        if case:
+            outputMeasSnr_Linear[batchIdx] = np.mean(np.divide(np.power(yGroundTruth[:, 1], 2), np.diag(R)[1]))
+        else:
+            outputMeasSnr_Linear[batchIdx] = np.mean(np.divide(np.power(yGroundTruth[:, 0], 2), np.diag(R)[0]))
 
         processNoiseMeanPower_dbm = 10*np.log10(np.mean(np.power(xGroundTruth - xPredictGroundTruth, 2), axis=0)) + 30
         autonomousStateMeanPower_dbm = 10*np.log10(np.mean(np.power(xPredictGroundTruth, 2), axis=0)) + 30
         processMeanSnr_db = autonomousStateMeanPower_dbm - processNoiseMeanPower_dbm
+        processMeanSnr_Linear[batchIdx] = np.power(10, np.divide(processMeanSnr_db[-2], 10))
 
         if enablePlot and batchIdx == 0:
             print('input meas snr: %d [db]; output meas snr: %d [db]' %(inputMeasSnr_db, outputMeanSnr_db))
@@ -112,7 +133,6 @@ def simFiltering2SmoothingErrorPower(desiredInputMeasNoise_dbm, desiredOutputMea
         tVec = np.arange(a,b)
 
         # cropping:
-        feature = -10
 
         errF = errF[a:b, feature]
         errS = errS[a:b, feature]
@@ -124,8 +144,11 @@ def simFiltering2SmoothingErrorPower(desiredInputMeasNoise_dbm, desiredOutputMea
         muS = muS[a:b, feature]
         errF2errS_db = errF2errS_db[a:b, feature]
 
-        meanErrF2ErrSPower_Linear[batchIdx] = np.divide(np.median(errF_W), np.median(errS_W))
-        meanErrS2ErrFPower_Linear[batchIdx] = np.divide(np.median(errS_W), np.median(errF_W))
+        # for meas noise I used median
+        #meanErrF2ErrSPower_Linear[batchIdx] = np.divide(np.median(errF_W), np.median(errS_W))
+        #meanErrS2ErrFPower_Linear[batchIdx] = np.divide(np.median(errS_W), np.median(errF_W))
+        meanErrF2ErrSPower_Linear[batchIdx] = np.divide(np.mean(errF_W), np.mean(errS_W))
+        meanErrS2ErrFPower_Linear[batchIdx] = np.divide(np.mean(errS_W), np.mean(errF_W))
         # wrong: meanErrF2ErrSPower_Linear[batchIdx] = np.mean(np.divide(errS_W, errF_W))
         #meanErrF2ErrS_dbm = 10*np.log10(meanErrF2ErrSPower_Linear[batchIdx]) + 30
 
@@ -149,4 +172,4 @@ def simFiltering2SmoothingErrorPower(desiredInputMeasNoise_dbm, desiredOutputMea
             plt.title('filtering 2 smoothing error power; mean: %d [db]' % watt2db(meanErrF2ErrSPower_Linear[batchIdx]))
             plt.show()
 
-    return meanErrF2ErrSPower_Linear, meanErrS2ErrFPower_Linear, outputMeasSnr_Linear
+    return meanErrF2ErrSPower_Linear, meanErrS2ErrFPower_Linear, outputMeasSnr_Linear, processMeanSnr_Linear
